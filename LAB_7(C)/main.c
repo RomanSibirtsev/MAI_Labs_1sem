26a3a92a890589db788453cae631e20601a469c5
Roman Sibirtsev <sibirtsevr1@gmail.com>
2022-10-29 00:34:55 +0300

    .

--- LAB_7(C)/main.c
+++ LAB_7(C)/main.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#define sign(x) ((x)<0 ? -(x) : (x))
+
+int min(int a, int b){
+  if (a > b){
+    return b;
+  }else{
+    return a;
+  }
+}
+
+int max(int a, int b){
+  if (a > b){
+    return a;
+  }else{
+    return b;
+  }
+}
+
+int sign(int a){
+  if (a < 0){
+    return -1;
+  }
+  else if (a == 0){
+    return 0;
+  }else{
+    return 1;
+  }
+}
+
+int abs(int a){
+  if (a < 0){
+    return -1*a;
+  }else{
+    return a;
+  }
+}
+
 int main(void)
 {
   int i = 13, j = 19, l = 14, pr = 1;
   for (int k = 0; k <= 50; ++k)
   {
-    if ((i + j + 10 >= 0) && (i + j + 20 <= 0))
+    printf("%d %d %d %d \n", k, i, j, l);
+    if ((i + j + 10 <= 0) && (i + j + 20 >= 0))
     {
       printf("YES, k = %d, i = %d, j = %d, l = %d", k, i, j, l);
       pr = 0;
@@ -16,8 +50,8 @@ int main(void)
     else
     {
       i = sign(i + 1) * abs(abs(k - j) - abs(i - l));
-      j = j % 20 + fmax(i % 20, fmin(j - k, l - k)) - 10;
-      l = k * (i + l) * (j + 2) * (l + 3) % 20;
+      j = j % 20 + max(i % 20, min(j - k, l - k)) - 10;
+      l = k * (i + 1) * (j + 2) * (l + 3) % 20;
     }
   }
   if (pr == 1)
