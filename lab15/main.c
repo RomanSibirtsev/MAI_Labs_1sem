5b7a3cde64a6d86ef5c643a8b702e3b318aaf437
Roman <56503839+RomanSibirtsev@users.noreply.github.com>
2022-12-10 09:02:19 +0000

    Update main.c

--- lab15/main.c
+++ lab15/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <limits.h>
 
-int main(void) {
-    int n;
-    int max_elem = INT_MIN, min_elem = INT_MAX;
-    int mi, mj;
-    int res = 0;
-    scanf("%d", &n);
+int n;
+
+int get_result (int a[n][n], int n);
+void input(int n);
+
+
+void input(int n){
     int a[n][n];
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             scanf("%d", &a[i][j]);
+        }
+    }
+    get_result(a, n);
+}
+
+int get_result (int a[n][n], int n) {
+    int max_elem = INT_MIN, min_elem = INT_MAX;
+    int mi, mj;
+    int res = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
             if (a[i][j] > max_elem) {
                 max_elem = a[i][j];
                 mj = j;
@@ -25,5 +37,12 @@ int main(void) {
         res += a[mi][i] * a[i][mj];
     }
     printf("%d\n", res);
+    return res;
+}
+
+
+int main(void) {
+    scanf("%d", &n);
+    input(n);
     return 0;
 }
