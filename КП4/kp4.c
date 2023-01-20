d571cb260c1bdd2d2866c78f6b3646707c0f5b7a
Roman <56503839+RomanSibirtsev@users.noreply.github.com>
2023-01-20 19:08:35 +0300

    Update kp4.c

--- КП4/kp4.c
+++ КП4/kp4.c
@@ -24,16 +24,20 @@ double sec_der(double x) {
   return res;
 }
 
-int main(void) {
-  double a = 0, b = 2;
-  double x = a, x_old, eps = mech_eps();
+double f2 (double x) {
+  return (1.2502 - 0.25*x*x*x);
+}
+
+void newton(double a, double b) {
+  double x = a, x_old = -1, eps = sqrt(mech_eps());
   while (x <= b) {
     if (fabs(func(x)*sec_der(x)) < (der(x)*der(x))) {
       x_old = x;
       x = x_old - func(x)/der(x);
       if (fabs(x - x_old) <= eps) {
-        printf("%f \n", x);
-        printf("%f \n", func(x));
+        printf("Newton's method\n");
+        printf("%.16f \n", x);
+        printf("%.16f \n", func(x));
         break;
       }
     }
@@ -42,4 +46,38 @@ int main(void) {
       break;
     }
   }
+}
+
+void half(double a, double b) {
+  double x = a, mid, eps = sqrt(mech_eps());
+  while (b - a > eps) {
+    mid = (a + b) / 2.;
+    if (func(a) * func(mid) < 0) b = mid;
+    if (func(mid) * func(b) < 0) a = mid;
+  }
+  x = (a + b) / 2;
+  printf("dichotomy method\n");
+  printf("%.16f \n", x);
+  printf("%.16f \n", func(x));
+}
+
+void iter(double a, double b) {
+    double x = a, x_old = -1, eps = sqrt(mech_eps());
+    while (fabs(x_old - x) > eps) {
+      x_old = x;
+      x = f2(x);
+    }
+    printf("iterations method\n");
+    printf("%.16f \n", x);
+    printf("%.16f \n", func(x));
+}
+
+int main(void) {
+  double a = 0, b = 2;
+  newton(a, b);
+  printf("\n");
+  half(a, b);
+  printf("\n");
+  iter(a, b);
+
 }
